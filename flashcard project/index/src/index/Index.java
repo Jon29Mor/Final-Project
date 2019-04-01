c6958efc4c457da5224cf4bed5f57613b4bf677d
Jon29Mor <47425420+Jon29Mor@users.noreply.github.com>
2019-04-01 14:21:37 -0400

    changes 4/1

--- flashcard project/index/src/index/Index.java
+++ flashcard project/index/src/index/Index.java
@@ -4,31 +4,73 @@
  * and open the template in the editor.
  */
 package index;
+
 import javax.swing.*;
 import java.lang.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.lang.Math.*;
 import java.util.Random;
+
 /**
  *
  * @author dstat
  */
-public class Index {
-    
-    public static void main(String[] args) {
+public class Index extends JFrame implements ActionListener {
+
     String[] questions = {"Q One", "Q Two", "Q Three"};
     String[] answers = {"A One", "A Two", "A Three"};
+    JLabel test = new JLabel("Hello");
+    JLabel question = new JLabel();
+    JLabel answer = new JLabel();
+    JLabel next = new JLabel("");
+    JButton getAnswer = new JButton("Answer");
+    JButton nextQuestion = new JButton("Next Question");
+    boolean run = true;
+    
     Random random = new Random();
-    for (int i = 0; i < 2; i++) {
-        int nextRandom = random.nextInt(3);
-        JLabel q = new JLabel();
+    int nextRandom = random.nextInt(3);
+    
+    
+
+    public Index(){
+        super("Flashcards");
+        setLayout(new GridLayout(4, 1));
         
+        for (int i = 0; i < questions.length; i++) {
+            question.setText(questions[nextRandom]);
         }
-    JFrame frame = new JFrame();
+        
+        add(question);
+        add(getAnswer);
+        add(answer);
+        add(nextQuestion);
+        
+        getAnswer.addActionListener(this);
+        nextQuestion.addActionListener(this);
+    }
+
+    public static void main(String[] args) {
+        Index frame = new Index();
         frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        while(run = true){
+            if(source == getAnswer){
+                answer.setText(answers[nextRandom]);
+                
+            }
+            if(source == nextQuestion){
+                answer = next;
+                question.setText(questions[nextRandom]);
+                
+            }
+        }
+    }
+
 }
