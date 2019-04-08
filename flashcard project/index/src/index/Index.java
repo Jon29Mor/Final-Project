a9198d7752fecaabe9c78205f0107af86f8586af
Jon29Mor <47425420+Jon29Mor@users.noreply.github.com>
2019-04-08 12:48:01 -0400

    Done

--- flashcard project/index/src/index/Index.java
+++ flashcard project/index/src/index/Index.java
@@ -6,10 +6,19 @@
 package index;
 
 import javax.swing.*;
-import java.lang.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.lang.Math.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -18,8 +27,13 @@ import java.util.Random;
  */
 public class Index extends JFrame implements ActionListener {
 
-    String[] questions = {"Q One", "Q Two", "Q Three"};
-    String[] answers = {"A One", "A Two", "A Three"};
+    static int randomNum = 0;
+    ArrayList<String> questions = new ArrayList();
+    ArrayList<String> answers = new ArrayList();
+    Path questionsPath = 
+            Paths.get("D:\\PTCwork\\Project Management\\Final-Project-Git\\Final-Project\\Questions.txt");
+    Path answersPath = 
+            Paths.get("D:\\PTCwork\\Project Management\\Final-Project-Git\\Final-Project\\Answers.txt");
     JLabel test = new JLabel("Hello");
     JLabel question = new JLabel();
     JLabel answer = new JLabel();
@@ -37,9 +51,66 @@ public class Index extends JFrame implements ActionListener {
         super("Flashcards");
         setLayout(new GridLayout(4, 1));
         
-        for (int i = 0; i < questions.length; i++) {
-            question.setText(questions[nextRandom]);
+        try{
+            //get file size
+            BasicFileAttributes fileSize = Files.readAttributes(questionsPath, BasicFileAttributes.class);
+            //get number of words in file
+            InputStream input1 = new BufferedInputStream(Files.newInputStream(questionsPath));
+            BufferedReader read = new BufferedReader(new InputStreamReader(input1));
+            String j = read.readLine();
+            while (j != null) {
+                questions.add(j);
+                j = read.readLine();
+            }
+            input1.close();
+            read.close();
+            
+            try{
+            //get file size
+            BasicFileAttributes fileSize2 = Files.readAttributes(answersPath, BasicFileAttributes.class);
+            //get number of words in file
+            InputStream input2 = new BufferedInputStream(Files.newInputStream(answersPath));
+            BufferedReader read1 = new BufferedReader(new InputStreamReader(input2));
+            String k = read1.readLine();
+            while (k != null) {
+                answers.add(k);
+                k = read1.readLine();
+            }
+            input2.close();
+            read1.close();
         }
+            catch(IllegalArgumentException a){
+            System.out.println(a);
+        }
+        catch(FileSystemNotFoundException b){
+            System.out.println(b);
+        }
+        catch(SecurityException c){
+            System.out.println(c);
+        }
+        catch(UnsupportedOperationException d){
+            System.out.println(d);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        }
+        catch(IllegalArgumentException a){
+            System.out.println(a);
+        }
+        catch(FileSystemNotFoundException b){
+            System.out.println(b);
+        }
+        catch(SecurityException c){
+            System.out.println(c);
+        }
+        catch(UnsupportedOperationException d){
+            System.out.println(d);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        //check
         
         add(question);
         add(getAnswer);
@@ -60,17 +131,17 @@ public class Index extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        while(run = true){
+        
             if(source == getAnswer){
-                answer.setText(answers[nextRandom]);
-                
+                answer.setText(answers.get(randomNum));
             }
             if(source == nextQuestion){
-                answer = next;
-                question.setText(questions[nextRandom]);
+                randomNum = random.nextInt(questions.size());
+                answer.setText("");
+                question.setText(questions.get(randomNum));
                 
             }
         }
     }
 
-}
+
